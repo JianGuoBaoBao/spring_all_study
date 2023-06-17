c3ef2144c333a4318e3faae0bab84dc0a0e14bcb
坚果宝宝 <791551390@qq.com>
2023-06-17 21:51:21 +0800

    eureka 配置

--- springCloud_study/springcloud-provider-dept/src/main/java/com/kuang/springcloud/controller/DeptController.java
+++ springCloud_study/springcloud-provider-dept/src/main/java/com/kuang/springcloud/controller/DeptController.java
@@ -2,6 +2,8 @@ package com.kuang.springcloud.controller;
 
 import com.kuang.springcloud.pojo.Dept;
 import com.kuang.springcloud.service.DeptService;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -9,13 +11,17 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.PostConstruct;
+import java.io.Serializable;
 import java.util.List;
 
 @RestController
-public class DeptController {
+public class DeptController implements Serializable {
 
     @Autowired
     private DeptService deptService;
+
+    @Autowired
+    private DiscoveryClient client; // 得到具体的微服务
 
     @PostMapping("/dept/add")
     public boolean addDept(Dept dept){
@@ -30,6 +36,28 @@ public class DeptController {
     @GetMapping("/dept/list")
     public List<Dept> queryAll(){
         return deptService.queryAll();
+    }
+
+
+    // 注册进来的微服务： 获取一些消息
+    @GetMapping("/dept/discovery")
+    public Object discovery(){
+        // 获取微服务列表清单
+        List<String> services = client.getServices();
+        System.out.println("discovery->services: " + services);
+
+        // 得到一个具体的微服务信息
+        List<ServiceInstance> instances = client.getInstances("SPRINGCLOUD-PROVIER-DEPT");
+        for (ServiceInstance instance : instances){
+            System.out.println(
+                    instance.getHost() + "\t"+
+                    instance.getPort() + "\t"+
+                    instance.getUri() + "\t"+
+                    instance.getServiceId()
+            );
+        }
+
+        return this.client;
     }
 
 }
